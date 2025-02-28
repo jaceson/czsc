d77297a9b865e59b4f6e208aaf3d6ac23e3a9615
wj <vickywangg@qiyi.com>
2025-02-28 10:30:18 +0800

    提交结果

--- daily_script.sh
+++ daily_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-rm -rf /Users/wj/czsc/data/log.json
-source /Users/wj/workspace/czsc/czsc_env/bin/activate
-cd /Users/wj/czsc
+rm -rf ~/czsc/data/log.json
+source ~/workspace/czsc/czsc_env/bin/activate
+cd ~/czsc
 python czsc_daily_stock.py
+if cat ./data/log.json | grep -q "logout success!"; then
+	git add .
+	current_datetime=$(date +%Y-%m-%d_%H:%M:%S)
+	git commit -m "${current_datetime} update daily stock"
+	git push
+fi
