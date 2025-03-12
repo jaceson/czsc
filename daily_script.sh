e1f79c6b2d2daec062040ee43dfab72ab25e2829
wj <vickywangg@qiyi.com>
2025-03-12 10:08:22 +0800

    dev 每日任务增加etf相关

--- daily_script.sh
+++ daily_script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 param1="$1"
 echo "参数: $param1"
-
+# 激活虚拟环境
 rm -rf ~/czsc/data/log.json
 source ~/workspace/czsc/czsc_env/bin/activate
+# 股票筛选
 cd ~/czsc
 python czsc_daily_stock.py
+# etf数据更新
+python czsc_auto_etf.py
+# etf数据写入数据库
+python czsc_sqlite.py
+# etf数据筛选
+python czsc_daily_etf.py
 #commit
 if [[ "$param1" == "push" ]]; then
 	current_datetime=$(date +%Y-%m-%d_%H:%M:%S)
