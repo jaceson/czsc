de778981d0251c193bd739d2759d57faf18f4d55
wj <vickywangg@qiyi.com>
2025-04-09 17:33:54 +0800

    etf update

--- daily_script.sh
+++ daily_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 param1="$1"
-echo "参数: $param1"
+param2="$2"
+echo "参数1: $param1"
+echo "参数2: $param2"
 # 激活虚拟环境
 rm -rf ~/czsc/data/log.json
 source ~/workspace/czsc/czsc_env/bin/activate
@@ -16,14 +18,16 @@ if [[ "$param1" == "push" ]]; then
 		git push
 	fi
 fi
-# etf数据更新
-python czsc_auto_etf.py
-# etf数据写入数据库
-python czsc_sqlite.py
-# etf数据筛选
-python czsc_daily_etf.py
+
 #commit
-if [[ "$param1" == "push" ]]; then
+if [[ "$param2" == "push" ]]; then
+	# etf数据更新
+	python czsc_auto_etf.py
+	# etf数据写入数据库
+	python czsc_sqlite.py
+	# etf数据筛选
+	python czsc_daily_etf.py
+
 	current_datetime=$(date +%Y-%m-%d_%H:%M:%S)
 	if grep -q "Stock Finished!" ~/czsc/data/log.json; then
 		git add .
