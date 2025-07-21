cf9d06ba80be52dcd04cb44d5dcb761f6110471c
wj <vickywangg@qiyi.com>
2025-07-21 13:46:30 +0800

    dev 长线转折点策略

---
+++ CZSCStragegy_LongTermTurnStrategy.py
@@ -0,0 +1,134 @@
+# coding: utf-8
+import os
+import sys
+from czsc_daily_util import *
+from lib.MyTT import *
+import pandas as pd
+import baostock as bs
+
+plus_list = []
+minus_list = []
+hold_days = 15
+ratio_map = {}
+for x in range(1,hold_days+1):
+    ratio_map[x] = []
+
+def get_longterm_turn_buy_point(symbol,df):
+    ndf = get_rps_data(df)
+    YIHAOC3 = (ndf['RPS50']>=87)
+    YIHAOC6 = (ndf['RPS120']>=90)
+    YIHAOC7 = ((ndf['RPS50']>=90) | (ndf['RPS120']>=90))
+    YIHAOC8 = (ndf['close']>=HHV(ndf['close'],70))
+    YIHAOC9 = (YIHAOC7 & YIHAOC8)
+    YIHAOC10 = (YIHAOC3 | YIHAOC6)
+    YIHAOC11 = ((LLV(ndf['low'],50)>LLV(ndf['low'],200)) & YIHAOC9)
+    YIHAOC12 = ((LLV(ndf['low'],30)>LLV(ndf['low'],120)) & YIHAOC9)
+    YIHAOC13 = (LLV(ndf['low'],20)>LLV(ndf['low'],50))
+    YIHAOC14 = (YIHAOC11 | YIHAOC12 | YIHAOC13)
+    YIHAOC16 = (COUNT(IF(ndf['high']<HHV(ndf['high'],80),0,1),10))
+    YIHAOC17 = (((ndf['close']>=HHV(ndf['close'],50)) | (ndf['high']>=HHV(ndf['high'],50))) & YIHAOC7)
+    YIHAOC18 = (YIHAOC16 | YIHAOC17)
+    YIHAOC19 = ((ndf['close']>MA(ndf['close'],20)) & (ndf['close']>MA(ndf['close'],200)) & (MA(ndf['close'],120)/MA(ndf['close'],200)>0.9))
+    YIHAOC21 = (COUNT(IF(ndf['close']>MA(ndf['close'],200),1,0),45))
+    YIHAOC23 = (COUNT(IF(ndf['close']>MA(ndf['close'],250),1,0),45))
+    YIHAOC24 = ((YIHAOC21>=2) & (YIHAOC21<45))
+    YIHAOC26 = (COUNT(IF(ndf['low']<MA(ndf['close'],200),1,0),45))
+    YIHAOC27 = ((YIHAOC26>0) & (YIHAOC21>2))
+    YIHAOC29 = (COUNT(IF(ndf['low']<MA(ndf['close'],250),1,0),45))
+    YIHAOC30 = ((YIHAOC29>0) & (YIHAOC23>2))
+    YIHAOC31 = (YIHAOC24 | YIHAOC27 | YIHAOC30)
+    YIHAOC32 = ((MA(ndf['close'],120)>=REF(MA(ndf['close'],120),10)) | (MA(ndf['close'],200)>=REF(MA(ndf['close'],200),10)))
+    YIHAOC33 = ((MA(ndf['close'],120)>=REF(MA(ndf['close'],120),15)) | (MA(ndf['close'],200)>=REF(MA(ndf['close'],200),15)))
+    YIHAOC34 = (YIHAOC32 | YIHAOC33)
+    YIHAOC35 = ((MA(ndf['close'],120)>=REF(MA(ndf['close'],120),10)) | (MA(ndf['close'],200)>=REF(MA(ndf['close'],200),10)))
+    YIHAOC36 = ((MA(ndf['close'],120)>=REF(MA(ndf['close'],120),15)) & (MA(ndf['close'],200)>=REF(MA(ndf['close'],200),15)))
+    YIHAOC37 = (YIHAOC35 | YIHAOC36)
+    YIHAOC38 = ((MA(ndf['close'],120)>MA(ndf['close'],200)) & YIHAOC34)
+    YIHAOC39 = ((HHV(ndf['high'],30)/LLV(ndf['low'],120)<1.50) & YIHAOC34)
+    YIHAOC40 = ((HHV(ndf['high'],30)/LLV(ndf['low'],120)<1.55) & YIHAOC37)
+    YIHAOC41 = ((HHV(ndf['high'],30)/LLV(ndf['low'],120)<1.65) & YIHAOC38 & YIHAOC9)
+    YIHAOC42 = (YIHAOC39 | YIHAOC40 | YIHAOC41)
+    YIHAOC43 = (HHV(ndf['high'],5)/HHV(ndf['high'],120)>0.85)
+    YIHAOC44 = ((HHV(ndf['high'],5)/HHV(ndf['high'],120)>0.8) & YIHAOC9)
+    YIHAOC45 = (ndf['close']/HHV(ndf['high'],10)>0.9)
+    YIHAOC46 = ((YIHAOC43 | YIHAOC44) & YIHAOC45)
+
+    buy_con = (YIHAOC10 & YIHAOC14 & YIHAOC18 & YIHAOC19 & YIHAOC31 & YIHAOC42 & YIHAOC46)
+    if not df[buy_con].empty:
+        selected_indexs = ndf[buy_con].index
+        for idx in selected_indexs:
+            buy_date = ndf['date'][idx]
+            print(symbol+" 长线反转日期："+buy_date)
+            start_index = df.iloc[df['date'].values == buy_date].index[0]
+            buy_price = df['close'].iloc[start_index]
+            max_val = -1000
+            for idx in range(start_index+1,start_index+hold_days+1):
+                if idx<len(df['date']):
+                    stock_close = df['close'].iloc[idx]
+                    ratio = round(100*(stock_close-buy_price)/buy_price,2)
+                    ratio_map[idx-start_index].append(ratio)
+                    max_val = max(max_val,ratio)
+
+            if max_val>0:
+                plus_list.append(max_val)
+            else:
+                minus_list.append(max_val)
+
+def print_console(s_plus_list,s_minus_list,s_ratio_map):
+    print("正收益次数："+str(len(s_plus_list)))
+    if len(s_minus_list)>0 or len(s_plus_list):
+        print("正收益占比："+str(round(100*len(s_plus_list)/(len(s_minus_list)+len(s_plus_list)),2))+"%")
+    total = 0
+    for x in range(0,len(s_plus_list)):
+        total += s_plus_list[x]
+    print("总的正收益："+str(total))
+
+    total = 0
+    for x in range(0,len(s_minus_list)):
+        total += s_minus_list[x]
+    print("总的负收益："+str(total))
+    
+    # 每天
+    for x in range(1,hold_days+1):
+        print("第 {} 天：".format(x))
+        res_list = s_ratio_map[x]
+        plus_num = 0
+        plus_val = 0
+        minus_num = 0
+        minus_val = 0
+        for idx in range(0,len(res_list)):
+            ratio = res_list[idx]
+            if ratio>0:
+                plus_num += 1
+                plus_val += ratio
+            else:
+                minus_num += 1
+                minus_val += ratio
+        print("     正收益次数："+str(plus_num))
+        if plus_num>0 or minus_num>0:
+            print("     正收益占比："+str(round(100*plus_num/(plus_num+minus_num),2))+"%")
+        print("     总的正收益："+str(plus_val))
+        print("     总的负收益："+str(minus_val))
+
+if __name__ == '__main__':
+    lg = bs.login()
+
+    start_date = "2020-01-01"
+    current_date = datetime.now()
+    current_date_str = current_date.strftime('%Y-%m-%d')    
+    df = get_stock_pd("sh.000001", start_date, current_date_str, 'd')
+    end_date = df['date'].iloc[-1]
+    
+    all_symbols  = get_daily_symbols()
+    for symbol in all_symbols:
+        # 打印进度
+        print("进度：{} / {}".format(all_symbols.index(symbol),len(all_symbols)))
+            
+        # if symbol != "sz.300264":
+        #     continue
+        df = get_stock_pd(symbol, start_date, current_date_str, 'd')
+        get_longterm_turn_buy_point(symbol,df)
+
+    print_console(plus_list,minus_list,ratio_map)
+        
+    bs.logout()
