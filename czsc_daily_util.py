071de7164fa2f118ef316e2ea6925603574c24dc
wj <vickywangg@qiyi.com>
2025-08-27 08:51:48 +0800

    update stock data

--- czsc_daily_util.py
+++ czsc_daily_util.py
@@ -338,6 +338,7 @@ def is_best_strategy_point(symbol,df,max_ratio=0.2):
                 else:
                     if len(selected_indexs)>1:
                         czsc_logger().info("     2）✅主力进场前一个日期："+str(df['date'].iloc[selected_indexs[-2]]))
+                        czsc_logger().info("     3）距离中高相差："+str(round(100*(high_price-last_zs.zg)/last_zs.zg),2)+"%")
                     # czsc_logger().info("     2）中枢区间："+last_zs.sdt.strftime("%Y-%m-%d")+"到"+last_zs.edt.strftime("%Y-%m-%d"))
                     # czsc_logger().info("     3）中枢中低："+str(last_zs.zd))
                     # czsc_logger().info("     4）距离中枢中低："+str(distance_pct)+"%")
