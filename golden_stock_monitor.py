90fe5f7d0abd6eeb7ca074c41f438c8fe7b5020d
wj <vickywangg@qiyi.com>
2026-01-29 13:03:41 +0800

    dev 删除日志

--- golden_stock_monitor.py
+++ golden_stock_monitor.py
@@ -59,7 +59,6 @@ def get_current_stock_price(symbol,tdx_api):
             # 0: 深市， 1: 沪市
             data = tdx_api.get_security_bars(4, market, code, 0, 10)
             df = pd.DataFrame(data)
-            print(df)
             if df is not None and not df.empty:
                 # 查找对应的股票
                 low_price = float(df['low'].iloc[-1])
