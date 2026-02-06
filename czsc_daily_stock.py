c0ad6dc67614960de48a53db992f161a4e73fbc5
wj <vickywangg@qiyi.com>
2026-02-06 17:37:08 +0800

    dev 去除其他策略

--- czsc_daily_stock.py
+++ czsc_daily_stock.py
@@ -482,8 +482,8 @@ def main():
     today = datetime.today()
     # 获取当前日期是星期几（0 表示星期一，6 表示星期日）
     weekday = today.weekday()
-    if weekday>=4:
-        daily_config = {'mline':True,'minion':True,'chaodi':True,'golden':True,'strong':True,'strategy':True,'buypoint':True,'chan':True,'bigfish':True}
+    # if weekday>=4:
+    #     daily_config = {'mline':True,'minion':True,'chaodi':True,'golden':True,'strong':True,'strategy':True,'buypoint':True,'chan':True,'bigfish':True}
     # 所有股票
     all_symbols  = get_daily_symbols()
     # 股票池
