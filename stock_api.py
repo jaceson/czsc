8fd003b4d70551d7eb20500e2a6b2b6127f2f5e9
wj <vickywangg@qiyi.com>
2026-03-25 14:15:50 +0800

    update data

--- stock_api.py
+++ stock_api.py
@@ -57,6 +57,10 @@ if not is_num_valid and not is_date_valid:
 def sqrt_val(a,b):
 	return round(math.sqrt(a*b),3)
 
+# 中间位
+def mid_val(a,b):
+    return round((a+b)/2.0,3)
+
 # 黄金分割线
 def gold_val_low(a,b):
 	# 0.382、 0.618
@@ -66,13 +70,19 @@ def gold_val_low(a,b):
 def gold_val_high(a,b):
 	# 0.382、 0.618
 	val = max(a,b)-min(a,b)
-	return val*0.628 + min(a,b)
+	return val*0.618 + min(a,b)
 
 def get_date_diff(a,b):
     date1 = datetime.strptime(a, "%Y-%m-%d")
     date2 = datetime.strptime(b, "%Y-%m-%d")
     diff = date2 - date1
     return diff.days
+
+def get_date_future(a,days):
+    delta = timedelta(days)
+    date = datetime.strptime(a, "%Y-%m-%d")
+    date = date + delta
+    return date.strftime("%Y-%m-%d")
 
 def gold_date_val(a,b):
     days = get_date_diff(a,b)
@@ -84,10 +94,14 @@ def gold_date_val(a,b):
 print("关键点位：")
 if is_num_valid:
     print("           1）平   方  根："+str(sqrt_val(low_val,high_val)))
-    print("           2）黄金分割高点："+str(gold_val_high(low_val,high_val)))
-    print("           3）黄金分割低点："+str(gold_val_low(low_val,high_val)))
+    print("           2）中   间  位："+str(mid_val(low_val,high_val)))
+    print("           3）黄金分割高点："+str(gold_val_high(low_val,high_val)))
+    print("           4）黄金分割低点："+str(gold_val_low(low_val,high_val)))
     print('\n')
 if is_date_valid:
-    print("           1）上涨天数："+str(get_date_diff(start_date,end_date)))
-    print("           2）调整天数："+str(get_date_diff(start_date,end_date)*0.628))
-    print("           3）黄金分割低点："+str(gold_date_val(start_date,end_date)))
+    days = get_date_diff(start_date,end_date)
+    print("           1）上涨天数："+str(days))
+    print("           2）中间位："+str(get_date_future(end_date,math.ceil(days*0.5))))
+    print("           3）最少黄金位："+str(get_date_future(end_date,math.ceil(days*0.382))))
+    print("           4）最多黄金位："+str(get_date_future(end_date,math.ceil(days*0.618))))
+    
